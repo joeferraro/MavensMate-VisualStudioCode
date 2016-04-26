0719357b97be6343f60137ca90ce2ee6ea4a7968
David Helmer <dhelmer@mavensconsulting.com>
2016-04-26 14:26:44 -0400

    fixed indentation (was 2)

--- test/mavensmate/client.test.ts
+++ test/mavensmate/client.test.ts
@@ -5,36 +5,36 @@ import * as vscode from 'vscode';
 import { MavensMateClient } from '../../src/mavensmate/client';
 
 suite("MavensMate Client", () => {
-  let mavensMateClientOptions = {
-    baseURL: 'http://localhost:55555'
-  }; 
-  let mavensMateClient = new MavensMateClient(mavensMateClientOptions);
-  suite("isAppAvailable", () => {
-    suite("server is up", () => {
-      setup(() => {
-        nock(mavensMateClientOptions.baseURL)
-          .get('/app/home/index')
-          .reply(200, 'OK');
-      });
-      
-      test("returns true", () => {
-        return mavensMateClient.isAppAvailable()
-          .then((isAvailable) =>{
-            expect(isAvailable).to.be.true;
-          });
-      });
+    let mavensMateClientOptions = {
+        baseURL: 'http://localhost:55555'
+    }; 
+    let mavensMateClient = new MavensMateClient(mavensMateClientOptions);
+    suite("isAppAvailable", () => {
+        suite("server is up", () => {
+            setup(() => {
+                nock(mavensMateClientOptions.baseURL)
+                .get('/app/home/index')
+                .reply(200, 'OK');
+            });
+
+            test("returns true", () => {
+                return mavensMateClient.isAppAvailable()
+                .then((isAvailable) =>{
+                    expect(isAvailable).to.be.true;
+                });
+            });
+        });
+
+        suite("server is down", () => {
+            test("returns an error", () => {
+                return mavensMateClient.isAppAvailable()
+                    .then((isAvailable) =>{
+                        expect().fail("Shouldn't be available");
+                    })
+                    .catch((requestError) => {
+                        expect(requestError).to.not.be(undefined);
+                    });
+            });
+        });
     });
-    
-    suite("server is down", () => {
-      test("returns an error", () => {
-        return mavensMateClient.isAppAvailable()
-          .then((isAvailable) =>{
-            expect().fail("Shouldn't be available");
-          })
-          .catch((requestError) => {
-            expect(requestError).to.not.be(undefined);
-          });
-      });
-    });
-  });
 });
