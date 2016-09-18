7d5d924d1c9916a5b25ad99f195dde475b38fd19
David Helmer <dhelmer@mavens.com>
2016-09-17 21:49:33 -0400

    Add Flush Logs

---
+++ src/mavensmate/commands/flushLogs.ts
@@ -0,0 +1,28 @@
+import { ClientCommand } from './clientCommand';
+import { BaseCommand } from './baseCommand';
+
+module.exports = class FlushLogs extends ClientCommand {
+    body: {
+        args: {
+            ui: boolean
+        }
+    }
+    static create(): BaseCommand {
+        return new FlushLogs();
+    }
+
+    constructor() {
+        super('Flush Logs');
+        this.id = 'flush-logs';
+        this.async = false;
+        this.body = {
+            args: {
+                ui: true
+            }
+        }
+    }
+
+    execute(): Thenable<any> {
+        return super.execute();
+    }
+}
