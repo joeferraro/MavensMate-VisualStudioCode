cb4697642eb9c62f0bacdacb4743a1ab6d542947
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-01-31 21:12:07 -0700

    joeferraro/MavensMate#838  Fixed null check (was failing when value === undefined)

--- src/mavensmate/projectSettings.ts
+++ src/mavensmate/projectSettings.ts
@@ -17,7 +17,7 @@ export class ProjectSettings {
         if(projectPath && !ProjectSettings._instances[projectPath]){
             let settingsPath = buildSettingsPath(projectPath);
             console.info(`Retrieving settings at path:  ${ settingsPath }`);
-            ProjectSettings._instances[projectPath] = file.open(settingsPath);            
+            ProjectSettings._instances[projectPath] = file.open(settingsPath);
         }
 
         return ProjectSettings._instances[projectPath];
@@ -28,7 +28,7 @@ export class ProjectSettings {
         if(!ProjectSettings._instances[projectPath]){
             ProjectSettings.getProjectSettings(projectPath);
         }
-        return ProjectSettings._instances[projectPath] !== null;
+        return ProjectSettings._instances[projectPath] != null;
     }
 }
 
